f4fa1e9bc7ee76ca9ada6936659f4bdb1c4f4bcc
Agrim <agrimsharma1714@gmail.com>
2022-12-18 19:49:22 +0530

    made a front-end folder, and deploying the back-end

---
+++ hardhat-project/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "a7e6caf4c7e64adeb6222ae8453b6cfa";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
