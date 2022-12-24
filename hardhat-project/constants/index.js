550f66dace92e2d699dc4fae46f0a297f538e115
Agrim <agrimsharma1714@gmail.com>
2022-12-24 23:30:46 +0530

    worked on abi, added address in both constants folder

--- hardhat-project/constants/index.js
+++ hardhat-project/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x58135c8bED3EB80f4aD40520d3AFc9142Fd0e8B7";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-dapp-theta.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
