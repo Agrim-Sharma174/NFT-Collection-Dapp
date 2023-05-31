fb90ea90100fb2b77e798bf7f9cfb620b5f0f7c6
Agrim <agrimsharma1714@gmail.com>
2023-05-31 15:49:34 +0530

    updated the testnet and constants folder in backend

--- hardhat-project/constants/index.js
+++ hardhat-project/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x58135c8bED3EB80f4aD40520d3AFc9142Fd0e8B7";
+const WHITELIST_CONTRACT_ADDRESS = "0x7aA1d459FbA9B4FB8cD714946241606Fb2F11d27";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-dapp-theta.vercel.app/api/";
 
